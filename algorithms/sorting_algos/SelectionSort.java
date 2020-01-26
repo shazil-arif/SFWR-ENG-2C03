d17680644c59170759831bbc17e09e2ee44bc45f
Shazil Arif <shazilarif94@gmail.com>
2020-01-25 19:05:20 -0500

    added recursive selection sort

--- algorithms/sorting_algos/SelectionSort.java
+++ algorithms/sorting_algos/SelectionSort.java
@@ -3,7 +3,7 @@ class SelectionSort{
         int[] keys = {45,12,54,22,90,24,90,100,58,34};
         System.out.println("Unsorted");
         PrintArray.print_arr(keys);
-        selection_sort(keys);
+        selection_sort_recursive(0,keys);
         System.out.println("Sorted");
         PrintArray.print_arr(keys);
     }
@@ -15,8 +15,21 @@ class SelectionSort{
             if(keys[i] < min){
                 min = keys[i];
             }
-
         }        
+    }
+    /* recursive implementation of selection sort
+    @param pointer The starting index of the sub array to sort
+    @param keys int values to sort */
+    public static void selection_sort_recursive(int pointer,int keys[]){
+        if(pointer >= keys.length) return;
+        int min_index = pointer;
+        for(int i = pointer; i < keys.length; i++){
+            if(keys[i] < keys[min_index] ){
+                min_index = i;
+            }
+        }
+        Swap.swap(keys, min_index, pointer);
+        selection_sort_recursive(pointer + 1, keys);
     }
     public static void print_arr(int keys[]){
         
